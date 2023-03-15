dc474272bce2199a2539b5b966dc18cacbf92c2d
sanyuanzai <931502949@qq.com>
2023-03-15 10:36:01 +0800

    chore:整体结构和工具安装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect, useCallback } from 'react'
+import type { FC, ReactNode } from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+interface Iprops {
+  children?: ReactNode
 }
-
-export default App;
+enum ColorSchemeMode {
+  light,
+  dark
+}
+const COLOR_THEME = [
+  {
+    name: 'light',
+    color: {
+      head: 'black',
+      bgPrimary: '#ee9b02',
+      txtPrimary: '#eeecec'
+    }
+  },
+  {
+    name: 'dark',
+    color: {
+      head: '#16beec',
+      bgPrimary: '#aaa',
+      txtPrimary: '#f7f74b'
+    }
+  }
+]
+const GlobalStyle = createGlobalStyle<any>`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  :root {
+    color-scheme: ${({ theme }) => theme.name || 'normal'};
+    --color-head:${({ theme }) => theme.color.head || 'inherit'};
+  }
+  h1{
+    color:var(--color-head)
+  }
+`
+const App: FC<Iprops> = () => {
+  const [mode, setMode] = useState<ColorSchemeMode>(ColorSchemeMode.dark)
+  const sysModeChangeHandler = useCallback((e: any) => {
+    if (e.matches) {
+      setMode(ColorSchemeMode.dark)
+    } else {
+      setMode(ColorSchemeMode.light)
+    }
+  }, [])
+  useEffect(() => {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    setMode(isDarkMode ? ColorSchemeMode.dark : ColorSchemeMode.light)
+    window
+      .matchMedia('(prefers-color-scheme:dark)')
+      .addEventListener('change', sysModeChangeHandler)
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme:dark)')
+        .removeEventListener('change', sysModeChangeHandler)
+    }
+  }, [])
+  return (
+    <ThemeProvider theme={COLOR_THEME[mode]}>
+      <GlobalStyle />
+      <h1>App</h1>
+    </ThemeProvider>
+  )
+}
+export default App
---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+export const COLOR_THEME = [
+  {
+    name: 'light',
+    color: {
+      head: 'black',
+      bgPrimary: '#ee9b02',
+      txtPrimary: '#eeecec'
+    }
+  },
+  {
+    name: 'dark',
+    color: {
+      head: '#16beec',
+      bgPrimary: '#aaa',
+      txtPrimary: '#f7f74b'
+    }
+  }
+]
